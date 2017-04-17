f819c5f60ef3acb9e90263e19c0b1a1a3733bf15
Tomas Gonzalez Vivo <tomas.gvivo@opinaiapanel.com>
2017-04-17 18:31:45 -0300

    Oops!

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-#!/usr/local/bin/node
-
-const Framework = require('./lib/Framework');
-const App = Framework.include('modules', 'App');
-
-let app = new App();
-app.execute();
+module.exports = require('./lib/Framework');
